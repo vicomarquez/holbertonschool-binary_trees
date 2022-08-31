a4d5573a106ddecb04e936242f0c731e5b0e2246
Marcelo <marcelorodb@gmail.com>
2022-08-31 16:42:50 -0700

    fix binary tree node ex1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,19 +7,18 @@
  * @value: value to put in the new node
  *
  * Return: pointer to the new node, or NULL on failure
- */
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
+
 	new = malloc(sizeof(binary_tree_t));
-	
 	if (!new)
 		return (NULL);
-	new-> = value;
+	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
 	return (new);
-
 }
