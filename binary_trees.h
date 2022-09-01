215fffe298628876f157c7bc96026a340238728f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-01 07:41:32 -0700

    task 6

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -46,5 +47,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
