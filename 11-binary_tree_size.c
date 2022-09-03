ec4b2f07cbd4bc11713609297534951d84e89384
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-02 18:19:47 -0700

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,11 +7,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-
 	if (tree == NULL)
 		return (0);
-
-	while (tree)
 		
-	return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
