efb70da7eb69816c7217b4a849ca2bd9fa94217c
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-04 16:52:07 -0700

    fix betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -54,26 +54,3 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
-/*
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- * Return: if tree is NULL, 0
- *
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left = 0;
-	size_t right = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-
-	if (left > right)
-		return (left + 1);
-	return (right + 1);
-}*/
