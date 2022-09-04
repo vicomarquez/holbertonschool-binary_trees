8831f81f78b28325bb98fb82491418dbcf1cafe3
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-04 16:36:57 -0700

    fix errors

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,11 +16,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	if (!left || !right)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
 
 	if (binary_tree_is_full(tree) && left == right)
 		return (1);
