eb38f83652fcee18c367f85b2e9feecf28591236
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-04 16:49:55 -0700

    fix errors

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,11 +13,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	if (left == right)
+		return (1);
+	return (0);
 
-
-	if (binary_tree_is_full(tree) && left == right)
+	if (binary_tree_is_full(tree))
 		return (1);
 	return (0);
 
@@ -41,10 +43,22 @@ int binary_tree_is_full(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: if tree is NULL, 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+/*
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: if tree is NULL, 0
- */
+ *
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
@@ -62,4 +76,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (left > right)
 		return (left + 1);
 	return (right + 1);
-}
+}*/
